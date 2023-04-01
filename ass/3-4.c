df4cea1ca80b5e3f26faba5c2998d6ab4a622903
mingreen0107 <71gnos@naver.com>
2023-04-01 20:12:22 +0900

    실습

--- ass/3-4.c
+++ ass/3-4.c
@@ -13,8 +13,8 @@ void main()
 	float f;
 	double d;
 
-	//pc = NULL;을 써서 코드가 돌아가게 한다면
-	//char 변수 c의 값이 변경된다.
+	//pc = NULL;을 써야지 코드가 실행된다.
+	//만약 포인터를 최기화 시켜주어 코드를 실행해보면 char 변수 c의 값이 변경된다.
 	printf("초기화하기전의 pc = %lu\n", pc);
 	pc = &c;
 	printf("초기화하기후의 pc = %lu\n", pc);
