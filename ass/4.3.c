5737063d466b41c16140feae674f50cbc5edcef8
mingreen0107 <71gnos@naver.com>
2023-04-12 21:53:32 +0900

    실습

---
+++ ass/4.3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define DataType int
+
+	typedef struct Node {
+	DataType data;
+	struct Node* link;
+} Node;
+
+int main() {
+
+	Node* head, * temp;
+
+	head = (Node*)malloc(sizeof(Node));
+	if (!head) exit(1);
+	head->data = 10;
+	head->link = NULL;
+
+	temp = (Node*)malloc(sizeof(Node));
+	if (!temp) exit(1);
+	temp->data = 20;
+	temp->link = NULL;
+	head->link = temp;
+
+	printf("head-->%d-->%d\n", head->data, head->link->data);
+	free(temp);
+	free(head);
+}
--- ass/p135-Q17.c
+++ ass/p135-Q17.c
@@ -1,32 +1,60 @@
-#include <stdio.h>
+<#include <stdio.h>
+#include <stdlib.h>
+#define DataType int
 
-void insert_first_C(Node** p2head, Node* new) {
-	if (*p2head == NULL) { // 빈 원형 연결리스트에 삽입
-		*p2head = new;
-		new->link = new;
-	}
-	else { // 맨 앞에 삽입
-		new->link = (*p2head)->link;
-		(*p2head)->link = new;
-	}
-}
+	typedef struct Node {
+	DataType data;
+	struct Node* link;
+} Node;
 
-void insert_last_C(Node** p2head, Node* new) {
-	if (*p2head == NULL) { // 빈 원형 연결리스트에 삽입
-		*p2head = new;
-		new->link = new;
-	}
-	else { // 맨 마지막에 삽입
-		new->link = (*p2head)->link;
-		(*p2head)->link = new;
-		*p2head = new; // insert_first_C에 여기만 추가
-	}
-}
 int main() {
 
+	Node* stu1, * stu2, * stu3, * stu4;
+	int n;
 
+	stu1 = (Node*)malloc(sizeof(Node));
+	if (!stu1)
+		exit(1);
+	stu1->data = 's';
+	stu1->link = NULL;
 
+	stu2 = (Node*)malloc(sizeof(Node));
+	if (!stu2)
+		exit(1);
+	stu2->data = 'm';
+	stu2->link = NULL;
+	stu1->link = stu2;
 
+	stu3 = (Node*)malloc(sizeof(Node));
+	if (!stu3)
+		exit(1);
+	stu3->data = 'j';
+	stu3->link = NULL;
+	stu2->link = stu3;
 
-	return 0;
-}
+	stu4 = (Node*)malloc(sizeof(Node));
+	if (!stu4)
+		exit(1);
+	stu4->data = 'b';
+	stu4->link = NULL;
+	stu3->link = stu4;
+
+	printf("%c - %c - %c - %c\n", stu1->data, stu1->link->data,
+		stu1->link->link->data, stu1->link->link->link->data);
+
+	scanf("%d", &n);
+
+	if (n == 1)
+		printf("%c - %c", stu1->link->link->data, stu1->link->link->link->data);
+	else if (n == 2)
+		printf("%c - %c", stu1->data, stu1->link->link->link->data);
+	else if (n == 3)
+		printf("%c - %c", stu1->link->data, stu1->link->link->link->data);
+	else
+		printf("%c - %c", stu1->data, stu1->link->link->data);
+
+	free(stu1);
+	free(stu2);
+	free(stu3);
+	free(stu4);
+} >
