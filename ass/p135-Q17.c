2df8f3ed9d4b1f740f2e34ec9bf19c113c509729
mingreen0107 <71gnos@naver.com>
2023-04-16 02:22:51 +0900

    실습

--- ass/p135-Q17.c
+++ ass/p135-Q17.c
@@ -4,14 +4,15 @@
 
 typedef struct Node { // 이름과 관련있는 노드 생성
 	char name[10];
+	struct Node* data;
 	struct Node* next;
 } Node;
 
-typedef struct List { // 이어주는 노드 생성
-	struct List* next;
+typedef struct List { // 연결하는 노드 생성
+	int count;
+	Node* head;
+	Node* tail;
 } List;
-
-//Node* ConnectNode(Node* name, N)
 
 Node* newNode(char* inputName) { // 이름을 저장하는 노드 생성
 	
@@ -23,11 +24,45 @@ Node* newNode(char* inputName) { // 이름을 저장하는 노드 생성
 
 	return inputname;
 }
+void setList(List* list) {
+	list->head = newNode(" ");
+	list->tail = newNode(" ");
+	list->count = 0;
+}
+void insert_node(Node** p2head, Node* new) {
+
+	if (*p2head == NULL) {
+		new->data = NULL;
+		*p2head = new;
+	}
+	else {
+		new->next = (*p2head)->next;
+		(*p2head)->data = new;
+	}
+}
+void connectNode(Node* p, List* list) {
+	Node* new = newNode(p);
+
+	if (list->count == 0) {
+		new->next = NULL;
+		new->data = list->head;
+	}
+	else {
+		new->data = list->head;
+		new->next = list->tail;
+	}
+	list->count++;
+}
+
 
 int main() {
 
 	Node* p;
+	List* list;
 	char inputName[10];
+	int i = 0;
+
+	setList(&list);
 
 	printf("학생의 이름을 입력하시오. (0을 입력하면 종료)\n");
 	while (1) {
@@ -38,13 +73,13 @@ int main() {
 			break;
 		else {
 			p = newNode(inputName);
-			
-			while (p) {
-				printf("%s ", p->name);
-				p = p->next;
+			connectNode(p, list);
 			}
 		}
 		
-
+	while (p)
+	{
+		printf("%s", p);
+		p = p->next;
 	}
 }
