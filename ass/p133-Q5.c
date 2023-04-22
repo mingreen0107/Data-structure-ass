f448f391d873b16aced24b3ff1f7aa368fe07e43
mingreen0107 <71gnos@naver.com>
2023-04-22 18:19:12 +0900

    실습

--- ass/p133-Q5.c
+++ ass/p133-Q5.c
@@ -1,52 +1,66 @@
+// ¿Ã∞‘ ø÷ æ»µ≈
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct Node {
 	char data;
 	struct Node* link;
+	int i;
 } Node;
 
-Node* merge(Node** A, Node** B) {
-	Node* C = (Node*)malloc(sizeof(Node));
-	int i;
-
-	for (i = 0; i < 6; i++) {
-		C
+Node* insert_node(Node** A, Node** B) {
+	char C[6] = { 0 };
+	int j = 0;
+	while ((*B)->i < 0) {
+		if ((*A)->i >= (*B)->i) {
+			C[j] = (*A)->data;
+			*A = (*A)->link;
+			j++;
+		}
+		else {
+			C[j] == (*B)->data;
+			*B = (*B)->link;
+			j++;
+		}
 	}
-
+	for (j = 0; j < 6; j++)
+		printf("%c", C[j]);
 }
 
 int main() {
-
-	Node* A, * a1, * a2, * B, * b1, * b2;
-
-	A = (Node*)malloc(sizeof(Node));
-	A->data = 'a';
-	A->link = NULL;
-
-	a1 = (Node*)malloc(sizeof(Node));
-	a1->data = 'c';
+	Node* a1 = (Node*)malloc(sizeof(Node));
+	a1->data = 'a';
 	a1->link = NULL;
-	A->link = a1;
+	a1->i = 3;
 
-	a2 = (Node*)malloc(sizeof(Node));
-	a2->data = 'e';
+	Node* a2 = (Node*)malloc(sizeof(Node));
+	a2->data = 'c';
 	a2->link = NULL;
+	a2->i = 2;
 	a1->link = a2;
 
-	B = (Node*)malloc(sizeof(Node));
-	B->data = 'q';
-	B->link = NULL;
+	Node* a3 = (Node*)malloc(sizeof(Node));
+	a3->data = 'e';
+	a3->link = NULL;
+	a3->i = 1;
+	a2->link = a3;
 
-	b1 = (Node*)malloc(sizeof(Node));
-	b1->data = 'u';
+	Node* b1 = (Node*)malloc(sizeof(Node));
+	b1->data = 'q';
 	b1->link = NULL;
-	B->link = b1;
+	b1->i = 3;
 
-	b2 = (Node*)malloc(sizeof(Node));
-	b2->data = 'e';
+	Node* b2 = (Node*)malloc(sizeof(Node));
+	b2->data = 'u';
 	b2->link = NULL;
+	b2->i = 2;
 	b1->link = b2;
 
-	merge(&A, &B);
+	Node* b3 = (Node*)malloc(sizeof(Node));
+	b3->data = 'e';
+	b3->link = NULL;
+	b2->i = 1;
+	b2->link = b3;
+
+	insert_node(&a1, &b1);
 }
