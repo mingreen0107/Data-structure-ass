2f29a969a6bf69f67fa3b1edd498daf23256108f
mingreen0107 <71gnos@naver.com>
2023-05-06 13:24:10 +0900

    실습

--- ass/3st-1.c
+++ ass/3st-1.c
@@ -11,15 +11,15 @@ int palindrome(char in_str[]) {
 		ch = in_str[i];
 		// 만약 ch가 스페이스거나 구두점이면
 		if () continue;
-		ch = tolower(ch); // ch를 소문자로 변경
-		; // 스택에 삽입한다.
+		// ch를 소문자로 변경 후 스택에 삽입
+		ch = tolower(ch); 
 	}
 	for (i = 0; i < len; i++) {
 		ch = in_str[i];
 		// 만약 ch가 스페이스거나 구두점이면
 		if () continue;
-		ch = tolower(ch); // ch를 소문자로 변경
-		chs = ; // 스택에서 문자를 꺼낸다
+		// ch를 소문자로 변경
+		ch = tolower(ch); 
 		if (ch != chs) return FALSE; // 실패
 	}
 	return TRUE; // 성공
