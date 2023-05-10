11fdbd871d61fda27ff16f5e0899a2e51fd1d4a1
mingreen0107 <71gnos@naver.com>
2023-05-10 19:54:29 +0900

    실습

--- ass/3st-1.c
+++ ass/3st-1.c
@@ -1,9 +1,42 @@
+// 회문 구두점, 스페이스, 대소문자 무시
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ArrayStack {
+	int top;
+	int stack[100];
+} ArratStack;
+
+// 회문이면 TRUE 반환 아니면 FALSE를 반환한다.
+int palindrome(char in_str[]) {
+	ArrayStack s;
+	int i;
+	char ch, chs;
+	int len = strlen(in_str);
+
+	; // 스택을 초기화하라
+	for (i = 0; i < len; i++) {
+		ch = in_str[i];
+		// 만약 ch가 스페이스거나 구두점이면
+		if () continue;
+		ch = tolower(ch); // ch를 소문자로 변경
+		; // 스택에 삽입한다.
+	}
+	for (i = 0; i < len; i++) {
+		ch = in_str[i];
+		// 만약 ch가 스페이스거나 구두점이면
+		if () continue;
+		ch = tolower(ch); // ch를 소문자로 변경
+		chs = ; // 스택에서 문자를 꺼낸다
+		if (ch != chs) return FALSE; // 실패
+	}
+	return TRUE; // 성공
+}
 
 int main()
 {
 
-	printf("%d", 1);
+	
 	
 	return 0;
 }
