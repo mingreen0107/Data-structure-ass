e5d833b78570b6c44f3e8d5ef2dd3d825859c9cd
mingreen0107 <71gnos@naver.com>
2023-05-10 20:12:10 +0900

    실습

---
+++ ass/7.1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ArraytStack {
+	int top;
+	int stack[100];
+} ArrayStack;
+// 스택 초기화
+void init_stack(ArrayStack* AS) {
+	AS->top = -1;
+}
+// 항목 출력
+void print_stack(ArrayStack* AS) {
+	printf("top->");
+	for (int i = AS->top; i >= 0; i--)
+		printf("%d", AS->stack[i]);
+	printf("\n");
+}
+void push(ArrayStack* AS, int data) {
+	return AS->stack[++AS->top] = data;
+}
+int pop(ArrayStack* AS) {
+	return AS->stack[AS->top--];
+}
+int peek(ArrayStack* AS) {
+	return AS->stack[AS->top];
+}
+int main() {
+	ArrayStack astack;
+	init_stack(&astack);
+	push(&astack, 10);
+	push(&astack, 20);
+	push(&astack, 30);
+	print_stack(&astack);
+	printf("top = %d\n", peek(&astack));
+	pop(&astack);
+	printf("top = %d\n", peek(&astack));
+	pop(&astack);
+	printf("top = %d", peek(&astack));
+	pop(&astack);
+	pop(&astack);
+}
