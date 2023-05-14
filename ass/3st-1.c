bbadf17eee0ee10d23918974a9636e3bad992d86
mingreen0107 <71gnos@naver.com>
2023-05-14 22:59:34 +0900

    실습

--- ass/3st-1.c
+++ ass/3st-1.c
@@ -1,12 +1,23 @@
 // 회문 구두점, 스페이스, 대소문자 무시
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct ArrayStack {
 	int top;
-	int stack[100];
+	char stack[100];
 } ArratStack;
 
+void init_stack(ArrayStack* AS) {
+	AS->top = -1;
+}
+int peek(ArrayStack* AS) {
+	return AS->stack[AS->top];
+}
+void push(ArrayStack* AS, int data) {
+	return AS->stack[++AS->top] = data;
+}
 // 회문이면 TRUE 반환 아니면 FALSE를 반환한다.
 int palindrome(char in_str[]) {
 	ArrayStack s;
@@ -14,20 +25,20 @@ int palindrome(char in_str[]) {
 	char ch, chs;
 	int len = strlen(in_str);
 
-	; // 스택을 초기화하라
+	init_stack(&s); // 스택을 초기화하라
 	for (i = 0; i < len; i++) {
 		ch = in_str[i];
 		// 만약 ch가 스페이스거나 구두점이면
-		if () continue;
+		if (ch ==' ' || ch == top) continue;
 		ch = tolower(ch); // ch를 소문자로 변경
-		; // 스택에 삽입한다.
+		push(&s, ch); // 스택에 삽입한다.
 	}
 	for (i = 0; i < len; i++) {
 		ch = in_str[i];
 		// 만약 ch가 스페이스거나 구두점이면
-		if () continue;
+		if (ch == ' ' || ch == top) continue;
 		ch = tolower(ch); // ch를 소문자로 변경
-		chs = ; // 스택에서 문자를 꺼낸다
+		chs = peek(&s); // 스택에서 문자를 꺼낸다
 		if (ch != chs) return FALSE; // 실패
 	}
 	return TRUE; // 성공
